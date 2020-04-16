f1d90de9eb64a746f235a30c95f30cae05c3d615
Chris Roman <Christopherroman85@gmail.com>
2020-04-16 10:08:01 -0500

    added welcome page, sessions, users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'blogs#index', as: 'home'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
 
   resources :blogs
   resources :articles
+  resources :users, only: [:new, :create]
 end
